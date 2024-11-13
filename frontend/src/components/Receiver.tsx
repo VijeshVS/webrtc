92178244f72e1c0975c62e2c037688b3841e2656
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-11-13 21:02:05 +0530

    first commit

---
+++ frontend/src/components/Receiver.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react"
+
+
+export const Receiver = () => {
+    
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:8080');
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'receiver'
+            }));
+        }
+        startReceiving(socket);
+    }, []);
+
+    function startReceiving(socket: WebSocket) {
+        const video = document.createElement('video');
+        const playButton = document.createElement('button');
+        playButton.innerText = 'Play Video';
+        playButton.onclick = () => video.play();
+        document.body.appendChild(video);
+        document.body.appendChild(playButton);
+
+        const pc = new RTCPeerConnection();
+        pc.ontrack = (event) => {
+            video.srcObject = new MediaStream([event.track]);
+        }
+
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'createOffer') {
+                pc.setRemoteDescription(message.sdp).then(() => {
+                    pc.createAnswer().then((answer) => {
+                        pc.setLocalDescription(answer);
+                        socket.send(JSON.stringify({
+                            type: 'createAnswer',
+                            sdp: answer
+                        }));
+                    });
+                });
+            } else if (message.type === 'iceCandidate') {
+                pc.addIceCandidate(message.candidate);
+            }
+        }
+    }
+
+    return <div>
+        
+    </div>
+}
